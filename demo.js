19367fe6620ef2d6eb3ea818fc5dccd06bc50cf9
DHARSHINI A <dharshiniannadurai0910@gmail.com>
2024-08-16 09:08:11 -0700

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,73 @@
+function abc()
+{
+    console.log("hello")
+}
+abc()
+console.log("--------")
+function add()
+{
+    a=10
+    b=20
+    console.log(a+b)
+}
+add()
+console.log("--------")
+function actor()
+{
+    factor="vijay"
+    fplayer="dhoni"
+    fmovie="siva"
+    console.log("factor:"+factor)
+    console.log("fplayer:"+fplayer)
+    console.log("fmovie:"+fmovie)
+}
+actor()
+//parameters
+function add(a,b)
+{
+    console.log(a+b)
+}
+add(10,38)
+console.log("___________")
+function area(l,b)
+{
+    console.log("area is:"+l*b)
+}
+area(6,8)
+//return
+function add(c,d)
+{
+    return(c+d)
+}
+var total=add(6,7)
+console.log(total)
+console.log("-----------------")
+//if else
+var homeworkfinish=false
+if(homeworkfinish)
+{
+    console.log("great job")
+}
+else
+{
+    console.log("finish your homework befre playing")
+}
+console.log("-----------------")
+//logical and ,or, not
+console.log(true&&false)
+console.log(true||false)
+console.log(!false)
+console.log("-----------------")
+var colour="green"
+if(colour=="green")
+{
+    console.log("go")
+}
+if(colour=="yellow")
+{
+    console.log("get ready")
+}
+if(colour=="red")
+{
+    console.log("stop")
+}
---
+++ javascript1.js
@@ -0,0 +1,25 @@
+var price=18
+console.log(price)
+var product="iphone"
+var tax=16
+console.log(product)
+console.log(price+tax)
+console.log("---------")
+var fruitname="apple"
+var count=16
+var price=199
+var total=count*price
+console.log(fruitname)
+console.log(total)
+console.log("---------")
+//datatypes
+//number
+console.log(typeof(10))
+//string
+console.log(typeof('apple'))
+//null
+var a
+console.log(a)
+//boolean
+var boolean=true
+console.log(boolean)
